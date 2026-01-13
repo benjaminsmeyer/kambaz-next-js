c198bae815972a11580e9769af83cac8d7ab145c
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2026-01-13 18:30:39 -0500

    Add navigation screen

--- app/(kambaz)/Account/Signin/page.tsx
+++ app/(kambaz)/Account/Signin/page.tsx
@@ -3,20 +3,10 @@ export default function Signin() {
   return (
     <div id="wd-signin-screen">
       <h3>Sign in</h3>
-      <input
-        placeholder="username"
-        className="wd-username"
-        defaultValue="alice"
-      />{" "}
+      <input placeholder="username" className="wd-username" /> <br />
+      <input placeholder="password" type="password" className="wd-password" />
       <br />
-      <input
-        placeholder="password"
-        type="password"
-        className="wd-password"
-        defaultValue="wonderland"
-      />
-      <br />
-      <Link href="/dashboard" id="wd-signin-btn">
+      <Link href="Profile" id="wd-signin-btn">
         Sign in
       </Link>
       <br />
