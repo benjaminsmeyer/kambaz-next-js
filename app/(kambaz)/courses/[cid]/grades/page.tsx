e49ad871034d95eb95d5242658f71ee82b57ee5f
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2026-01-14 14:18:40 -0500

    Add default quiz, grades, piazza, and zoom pages

---
+++ app/(kambaz)/courses/[cid]/grades/page.tsx
@@ -0,0 +1,7 @@
+export default function Grades() {
+  return (
+    <div id="wd-grades">
+      <h3 id="wd-grades-title">Grades</h3>
+    </div>
+  );
+}
---
+++ app/(kambaz)/courses/[cid]/zoom/page.tsx
@@ -0,0 +1,7 @@
+export default function Zoom() {
+  return (
+    <div id="wd-zoom">
+      <h3 id="wd-zoom-title">Zoom</h3>
+    </div>
+  );
+}
