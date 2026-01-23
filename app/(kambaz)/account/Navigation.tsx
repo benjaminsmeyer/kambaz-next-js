a67360e9865c4f7c4da3c66ca62d5280fa94c8b1
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2026-01-22 22:05:55 -0500

    a2 CSS and Bootstrap

--- app/(kambaz)/account/Navigation.tsx
+++ app/(kambaz)/account/Navigation.tsx
@@ -1,10 +1,45 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ListGroup } from "react-bootstrap";
+
 export default function AccountNavigation() {
+  const pathname = usePathname();
+  const isActive = (href: string) => pathname.startsWith(href);
+
   return (
-    <div id="wd-account-navigation">
-      <Link href="signin"> Signin </Link> <br />
-      <Link href="signup"> Signup </Link> <br />
-      <Link href="profile"> Profile </Link> <br />
-    </div>
+    <ListGroup
+      id="wd-account-navigation"
+      className="wd rounded-0 fs-5"
+      variant="flush"
+    >
+      <Link
+        href="/account/signin"
+        id="wd-account-signin-link"
+        className={`list-group-item list-group-item-action border-0 ${
+          isActive("/account/signin") ? "active" : "text-danger"
+        }`}
+      >
+        Signin
+      </Link>
+      <Link
+        href="/account/signup"
+        id="wd-account-signup-link"
+        className={`list-group-item list-group-item-action border-0 ${
+          isActive("/account/signup") ? "active" : "text-danger"
+        }`}
+      >
+        Signup
+      </Link>
+      <Link
+        href="/account/profile"
+        id="wd-account-profile-link"
+        className={`list-group-item list-group-item-action border-0 ${
+          isActive("/account/profile") ? "active" : "text-danger"
+        }`}
+      >
+        Profile
+      </Link>
+    </ListGroup>
   );
 }
