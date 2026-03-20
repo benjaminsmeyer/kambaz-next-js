4994f29b04ae9fa84bb0163f28ad716afb7007b5
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2026-03-20 16:54:04 -0400

    ch5 lab 5

--- app/labs/lab5/WorkingWithArrays.tsx
+++ app/labs/lab5/WorkingWithArrays.tsx
@@ -4,7 +4,13 @@ import { FormControl } from "react-bootstrap";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export default function WorkingWithArrays() {
   const API = `${HTTP_SERVER}/lab5/todos`;
-  const [todo, setTodo] = useState({ id: "1" });
+  const [todo, setTodo] = useState({
+    id: "1",
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-09-09",
+    completed: false,
+  });
   return (
     <div id="wd-working-with-arrays">
       <h3>Working with Arrays</h3>
@@ -59,6 +65,58 @@ export default function WorkingWithArrays() {
         className="w-50"
         onChange={(e) => setTodo({ ...todo, id: e.target.value })}
       />
+      <hr />
+      <h3>Updating an Item in an Array</h3>
+      <a
+        href={`${API}/${todo.id}/title/${todo.title}`}
+        className="btn btn-primary float-end"
+      >
+        Update Todo
+      </a>
+      <FormControl
+        defaultValue={todo.id}
+        className="w-25 float-start me-2"
+        onChange={(e) => setTodo({ ...todo, id: e.target.value })}
+      />
+      <FormControl
+        defaultValue={todo.title}
+        className="w-50 float-start"
+        onChange={(e) => setTodo({ ...todo, title: e.target.value })}
+      />
+      <br />
+      <br />
+      <div className="d-flex gap-2 mb-2">
+        <FormControl
+          id="wd-todo-description"
+          defaultValue={todo.description}
+          className="flex-grow-1"
+          onChange={(e) => setTodo({ ...todo, description: e.target.value })}
+        />
+        <a
+          id="wd-update-todo-description"
+          href={`${API}/${todo.id}/description/${encodeURIComponent(
+            todo.description,
+          )}`}
+          className="btn btn-primary align-self-start"
+        >
+          Describe Todo ID = {todo.id}
+        </a>
+      </div>
+      <a
+        id="wd-update-todo-completed"
+        href={`${API}/${todo.id}/completed/${todo.completed}`}
+        className="btn btn-primary float-end"
+      >
+        Complete Todo ID = {todo.id}
+      </a>
+      <div className="mb-2">
+        <input
+          id="wd-todo-completed"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) => setTodo({ ...todo, completed: e.target.checked })}
+        />
+      </div>
       <hr />
     </div>
   );
